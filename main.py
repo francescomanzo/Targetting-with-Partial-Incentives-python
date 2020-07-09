4a7775882d1ac5d328305055459975f0a389ea57
francescomanzo <francescomanzo@outlook.com>
2020-07-09 11:05:23 +0200

    Added chart and normal distribution

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import snap
 import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed()
 
 def TPI(G, threshold):
     s = {v.GetId(): 0 for v in G.Nodes()}
@@ -40,19 +43,72 @@ def deferredDecision(G, probs):
             graph.DelEdge(src, dst)
     return graph
 
-np.random.seed(8)
+
 G = snap.LoadEdgeList(snap.PUNGraph, 'email-Eu-core.txt', 0, 1)
-threshold = {v.GetId(): 5 for v in G.Nodes()}
 
-probs = {(e.GetSrcNId(), e.GetDstNId()): np.random.uniform() for e in G.Edges()}
+const_x = [i for i in range(1, 10)]
+frac_x = [i/10 for i in range(1, 10)]
 
-sum = 0
-for i in range(0, 10):
-    g_copy = deferredDecision(G, probs)
-    s = TPI(g_copy, threshold)
-    active = [key for key, value in s.items() if value >= threshold[key]]
-    sum = sum + len(active)
-average = sum/10
+const_uniform_y = []
+const_normal_y = []
 
-print(average)
+frac_uniform_y = []
+frac_normal_y = []
 
+uniform_probs = {(e.GetSrcNId(), e.GetDstNId()): np.random.uniform() for e in G.Edges()}
+normal_probs = {(e.GetSrcNId(), e.GetDstNId()): 0.5**np.abs(np.random.normal()) for e in G.Edges()}
+
+for i in range(1, 10):
+    constant_threshold = {v.GetId(): i for v in G.Nodes()}
+    fraction_threshold = {v.GetId(): v.GetDeg()*i/10 for v in G.Nodes()}
+
+    const_uniform_sum = 0
+    const_normal_sum = 0
+    frac_uniform_sum = 0
+    frac_normal_sum = 0
+
+    for j in range(0, 10):
+        g_uniform = deferredDecision(G, uniform_probs)
+        g_normal = deferredDecision(G, normal_probs)
+        
+        s = TPI(g_uniform, constant_threshold)
+        active = [key for key, value in s.items() if value >= constant_threshold[key]]
+        const_uniform_sum = const_uniform_sum + len(active)
+
+        s = TPI(g_normal, constant_threshold)
+        active = [key for key, value in s.items() if value >= constant_threshold[key]]
+        const_normal_sum = const_normal_sum + len(active)
+
+        s = TPI(g_uniform, fraction_threshold)
+        active = [key for key, value in s.items() if value >= fraction_threshold[key]]
+        frac_uniform_sum = frac_uniform_sum + len(active)
+
+        s = TPI(g_normal, fraction_threshold)
+        active = [key for key, value in s.items() if value >= fraction_threshold[key]]
+        frac_normal_sum = frac_normal_sum + len(active)
+
+    const_uniform_avg = const_uniform_sum / 10
+    const_normal_avg = const_normal_sum / 10
+    frac_uniform_avg = frac_uniform_sum / 10
+    frac_normal_avg = frac_normal_sum / 10
+
+    const_uniform_y.append(const_uniform_avg)
+    const_normal_y.append(const_normal_avg)
+    
+    frac_uniform_y.append(frac_uniform_avg)
+    frac_normal_y.append(frac_normal_avg)
+
+plt.xlabel('Threshold')
+plt.ylabel('Size')
+
+plt.plot(const_x, const_uniform_y, marker='.', label='Uniform probs')
+plt.plot(const_x, const_normal_y, marker='.', label='Normal probs')
+plt.title('Constant threshold')
+plt.legend(title='Probability distribution', loc='upper right', fontsize='small', fancybox=True)
+plt.show()
+
+plt.plot(frac_x, frac_uniform_y, marker='.', label='Uniform probs')
+plt.plot(frac_x, frac_normal_y, marker='.', label='Normal probs')
+plt.title('Degree proportional threshold')
+plt.legend(title='Probability distribution', loc='upper right', fontsize='small', fancybox=True)
+plt.show()
